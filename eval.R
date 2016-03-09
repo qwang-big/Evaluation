f298adf6b42999aaf7ac6e92e3f6a4768e16d067
saidbleik <saidbleik@hotmail.com>
2016-03-08 21:01:53 -0500

    c4

--- eval.R
+++ eval.R
@@ -14,6 +14,9 @@ Evaluate = function(actual=NULL, predicted=NULL, cm=NULL){
   if(is.null(cm)) {
     actual = actual[!is.na(actual)]
     predicted = predicted[!is.na(predicted)]
+    f = factor(union(unique(actual), unique(predicted)))
+    actual = factor(actual, levels = levels(f))
+    predicted = factor(predicted, levels = levels(f))
     cm = as.matrix(table(Actual=actual, Predicted=predicted))
   }
   
